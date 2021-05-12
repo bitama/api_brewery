2383c1772db9032b3496cd5ce6cd0d3793e1119c
nyibizipatrick <niyibizipatrick1@gmail.com>
2021-05-11 23:14:03 -0500

    Api UsEffect_demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+import React, { useState,useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  useEffect(() => {
+    console.log("useEffect")
+    axios.get(`https://api.openbrewerydb.org/breweries?by_city=${city}`)
+      .then(res => {
+        console.log(res)
+        setBreweries(res.data)
+      })
+      .catch(errors => console.log(errors))
+},[])
+  
+  const [breweries, setBreweries] = useState([])
+  const[city,setCity] = useState("chicago")
+  const fetchBreweryInfo = (event) => {
+    axios.get(`https://api.openbrewerydb.org/breweries?by_city=${city}`)
+      .then(res => {
+        console.log(res)
+        setBreweries(res.data)
+      })
+      .catch(errors=>console.log(errors))
+  //   fetch("https://api.openbrewerydb.org/breweries")
+  //     .then(response => response.json())
+  //     .then(response => {
+  //       console.log(response);
+  //       setBreweries(response);
+  // })
+  //     .catch(errors=>console.log(errors)) 
+        
+      }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div container>
+        <input className="form-control m-5 mx-auto center w-50" type="text" placeholder="city" onChange={(event)=>setCity(event.target.value)}/>
+      </div>
+      <button className="btn btn-primary btn-lg mt-1" onClick={fetchBreweryInfo}>Fetch Info information</button>
+      {
+        breweries.map((item, i) => <h1 key={i}>{item.name},{item.city}</h1>)
+      }
+      
     </div>
   );
-}
 
+}
 export default App;
